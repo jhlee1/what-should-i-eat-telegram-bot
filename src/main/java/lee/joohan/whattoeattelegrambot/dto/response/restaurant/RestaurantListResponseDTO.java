ca15e983accd299936e371f5fb721280c979ebeb
Joohan Lee <joohan33@gmail.com>
2020-06-07 12:36:50 +0900

    - CORS 설정 추가

--- src/main/java/lee/joohan/whattoeattelegrambot/dto/response/restaurant/RestaurantListResponseDTO.java
+++ src/main/java/lee/joohan/whattoeattelegrambot/dto/response/restaurant/RestaurantListResponseDTO.java
@@ -1,5 +1,8 @@
 package lee.joohan.whattoeattelegrambot.dto.response.restaurant;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import lee.joohan.whattoeattelegrambot.domain.Menu;
 import lee.joohan.whattoeattelegrambot.domain.Restaurant;
 import lombok.Getter;
 
@@ -11,9 +14,26 @@ import lombok.Getter;
 public class RestaurantListResponseDTO {
   private String restaurantId;
   private String name;
+  private String address;
+  private List<MenuDTO> menus;
 
   public RestaurantListResponseDTO(Restaurant restaurant) {
     restaurantId = restaurant.getId().toHexString();
     name = restaurant.getName();
+    address = restaurant.getAddress();
+    menus = restaurant.getMenus().stream()
+        .map(MenuDTO::new)
+        .collect(Collectors.toList());
+  }
+
+  @Getter
+  public static class MenuDTO {
+    private String name;
+    private int price;
+
+    public MenuDTO(Menu menu) {
+      name = menu.getName();
+      price = menu.getPrice();
+    }
   }
 }
